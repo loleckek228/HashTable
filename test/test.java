d4d0e7e60f22a0b5f7718c2d6b6a63b55e56d958
loleckek228 <george-valiev@yandex.ru>
2018-05-30 16:32:41 +0300

    test

--- test/test.java
+++ test/test.java
@@ -1,9 +1,4 @@
-import Hash.HashTable;
-import com.sun.org.apache.xml.internal.security.algorithms.implementations.IntegrityHmac;
 import org.junit.Test;
-
-import java.util.HashSet;
-import java.util.Hashtable;
 
 class tests {
 
